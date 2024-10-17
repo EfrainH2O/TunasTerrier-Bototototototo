af4361727cc0c8c4c841e03ca63ddbe80892f919
EfrainH2O <112781053+EfrainH2O@users.noreply.github.com>
2024-10-16 23:42:20 -0600

    Tab
    Tab

--- RoBorregos/RoBorregos.ino
+++ RoBorregos/RoBorregos.ino
@@ -1,4 +1,4 @@
-  //Pre - Setup
+//Pre - Setup
     // - LED RGB
       #define ledRojo 7
       #define ledVerde 6
@@ -46,7 +46,7 @@
       #define LeftEncoder 20
       volatile byte LeftPulses;
       void Lcount(){LeftPulses++;}
-    // - InfraRed
+    // - InfraRed 
       const int NInfraRedSensor = 5;
       int InfraRedSensors[] = {23, 25, 27, 29, 31};
       bool InfraRedValues[NInfraRedSensor];
@@ -57,229 +57,219 @@
       #define servoRight 9
       #define servoLeft 10
 
-void calibraSensorRGB(){
-  // Variables para la Medida del Ancho de Pulso de Color
-  int redPW = 0;
-  int greenPW = 0;
-  int bluePW = 0;
-  // Lee el ancho de pulso de ROJO
-  redPW = getRedPW();
-  delay(200);
-  // Lee el ancho de pulso de VERDE
-  greenPW = getGreenPW();
-  delay(200);
-  // Lee el ancho de pulso de AZUL
-  bluePW = getBluePW();
-  delay(200);
-  // Imprimir los valores en el Serial Monitor
-  Serial.print("Red PW = ");
-  Serial.print(redPW);
-  Serial.print(" - Green PW = ");
-  Serial.print(greenPW);
-  Serial.print(" - Blue PW = ");
-  Serial.println(bluePW);
-}
-
-void leeSensorRGB(){
-  // Variables para la Medida del Ancho de Pulso de Color
-  int redPW = 0;
-  int greenPW = 0;
-  int bluePW = 0;
-  // Variables para el Valor Final del Color
-  int redValue;
-  int greenValue;
-  int blueValue;
-  // Lee el ancho de pulso de ROJO
-  redPW = getRedPW();
-  // Mapea el valor desde 0-255
-  redValue = map(redPW, redMin, redMax, 255, 0);
-  delay(200);
-  // Lee el ancho de pulso de VERDE
-  greenPW = getGreenPW();
-  // Mapea el valor desde 0-255
-  greenValue = map(greenPW, greenMin, greenMax, 255, 0);
-  delay(200);
-  // Lee el ancho de pulso de AZUL
-  bluePW = getBluePW();
-  // Mapea el valor desde 0-255
-  blueValue = map(bluePW, blueMin, blueMax, 255, 0);
-  delay(200);
-  //
-  // Imprimir los valores finales en el Serial Monitor
-  Serial.print("Red Value = ");
-  Serial.print(redValue);
-  Serial.print(" - Green Value = ");
-  Serial.print(greenValue);
-  Serial.print(" - Blue Value = ");
-  Serial.println(blueValue);
-  ejecutaLedRGB(redValue, greenValue, blueValue);
-}
-
-int getRedPW(){
-  // Ajustar el sensor a leer unicamente ROJO
-  digitalWrite(S2, LOW);
-  digitalWrite(S3, LOW);
-  int PW;
-  // Lee la salida del ancho de pulso
-  PW = pulseIn(SensorOut, LOW);
-  return PW; // Regresar el valor obtenido
-}
-
-int getGreenPW(){
-  // Ajustar el sensor a leer unicamente VERDE
-  digitalWrite(S2, HIGH);
-  digitalWrite(S3, HIGH);
-  int PW;
-  // Lee la salida del ancho de pulso
-  PW = pulseIn(SensorOut, LOW);
-  return PW; // Regresar el valor obtenido
-}
-
-int getBluePW(){
-  // Ajustar el sensor a leer unicamente AZUL
-  digitalWrite(S2, LOW);
-  digitalWrite(S3, HIGH);
-  int PW;
-  // Lee la salida del ancho de pulso
-  PW = pulseIn(SensorOut, LOW);
-  return PW; // Regresar el valor obtenido
-}
-
-void ejecutaLedRGB(int R, int G, int B){
-  analogWrite(ledRojo, R); // Pasar el valor obtenido de ROJO al led RGB
-  analogWrite(ledVerde, G); // Pasar el valor obtenido de VERDE al led RGB
-  analogWrite(ledAzul, B);  // Pasar el valor obtenido de AZUL al led RGB
-  delay(300);
-}
-
-long findDistance(int triggerPin, int echoPin){
-  pinMode(triggerPin, OUTPUT);  
-  digitalWrite(triggerPin, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(triggerPin, LOW);
-  pinMode(echoPin, INPUT);
-  long distance = pulseIn(echoPin, HIGH, 50000)/58.2;
-  return distance;
-} 
-
-bool IsBall(){
-  float red, green, blue;
-  tcs.setInterrupt(false);
-  delay(60);  
-  tcs.getRGB(&red, &green, &blue);
-  tcs.setInterrupt(true);  
- /* analogWrite(ledRojo, red);
-  analogWrite(ledVerde, green);
-  analogWrite(ledAzul, blue);*/
-  Serial.print(red); Serial.print(" "); Serial.print(green); Serial.print(" "); Serial.println(blue);
-  return blue > green && red < blue;
-
-
-
-}
-
-void Drive(int left, int right){
-  //Set Motor Direction
-  if(left > 0){
-    digitalWrite(LeftA, HIGH);
-    digitalWrite(LeftB, LOW);
-  }else if(left < 0){
-    digitalWrite(LeftA, LOW);
-    digitalWrite(LeftB, HIGH);
-  }
-  else{
-    digitalWrite(LeftA, LOW);
-    digitalWrite(LeftB, LOW);
+// - Subsistemas
+  
+  void calibraSensorRGB(){
+    // Variables para la Medida del Ancho de Pulso de Color
+    int redPW = 0;
+    int greenPW = 0;
+    int bluePW = 0;
+    // Lee el ancho de pulso de ROJO
+    redPW = getRedPW();
+    delay(200);
+    // Lee el ancho de pulso de VERDE
+    greenPW = getGreenPW();
+    delay(200);
+    // Lee el ancho de pulso de AZUL
+    bluePW = getBluePW();
+    delay(200);
+    // Imprimir los valores en el Serial Monitor
+    Serial.print("Red PW = ");
+    Serial.print(redPW);
+    Serial.print(" - Green PW = ");
+    Serial.print(greenPW);
+    Serial.print(" - Blue PW = ");
+    Serial.println(bluePW);
   }
 
-  if(right > 0){
-    digitalWrite(RightA, HIGH);
-    digitalWrite(RightB, LOW);
-  }else if(right < 0){
-    digitalWrite(RightA, LOW);
-    digitalWrite(RightB, HIGH);
+  void leeSensorRGB(){
+    // Variables para la Medida del Ancho de Pulso de Color
+    int redPW = 0;
+    int greenPW = 0;
+    int bluePW = 0;
+    // Variables para el Valor Final del Color
+    int redValue;
+    int greenValue;
+    int blueValue;
+    // Lee el ancho de pulso de ROJO
+    redPW = getRedPW();
+    // Mapea el valor desde 0-255
+    redValue = map(redPW, redMin, redMax, 255, 0);
+    delay(200);
+    // Lee el ancho de pulso de VERDE
+    greenPW = getGreenPW();
+    // Mapea el valor desde 0-255
+    greenValue = map(greenPW, greenMin, greenMax, 255, 0);
+    delay(200);
+    // Lee el ancho de pulso de AZUL
+    bluePW = getBluePW();
+    // Mapea el valor desde 0-255
+    blueValue = map(bluePW, blueMin, blueMax, 255, 0);
+    delay(200);
+    //
+    // Imprimir los valores finales en el Serial Monitor
+    Serial.print("Red Value = ");
+    Serial.print(redValue);
+    Serial.print(" - Green Value = ");
+    Serial.print(greenValue);
+    Serial.print(" - Blue Value = ");
+    Serial.println(blueValue);
+    ejecutaLedRGB(redValue, greenValue, blueValue);
   }
-  else{
-    digitalWrite(RightA, LOW);
-    digitalWrite(RightB, LOW);
+
+  int getRedPW(){
+    // Ajustar el sensor a leer unicamente ROJO
+    digitalWrite(S2, LOW);
+    digitalWrite(S3, LOW);
+    int PW;
+    // Lee la salida del ancho de pulso
+    PW = pulseIn(SensorOut, LOW);
+    return PW; // Regresar el valor obtenido
   }
 
-  //Set Motor Velocity
-  left = abs(left);
-  right = abs(right);
-  left = left > 1 ? 1: left;
-  right = right > 1 ? 1: right;
+  int getGreenPW(){
+    // Ajustar el sensor a leer unicamente VERDE
+    digitalWrite(S2, HIGH);
+    digitalWrite(S3, HIGH);
+    int PW;
+    // Lee la salida del ancho de pulso
+    PW = pulseIn(SensorOut, LOW);
+    return PW; // Regresar el valor obtenido
+  }
 
-  int LeftWheel = map(left, 0, 1, 0, 255);
-  int RightWheel = map(right, 0, 1, 0, 255);
-  analogWrite(LeftPow, LeftWheel);
-  analogWrite(RightPow, RightWheel);
-}
+  int getBluePW(){
+    // Ajustar el sensor a leer unicamente AZUL
+    digitalWrite(S2, LOW);
+    digitalWrite(S3, HIGH);
+    int PW;
+    // Lee la salida del ancho de pulso
+    PW = pulseIn(SensorOut, LOW);
+    return PW; // Regresar el valor obtenido
+  }
 
-void UpdateInfraRedSensors(){
+  void ejecutaLedRGB(int R, int G, int B){
+    analogWrite(ledRojo, R); // Pasar el valor obtenido de ROJO al led RGB
+    analogWrite(ledVerde, G); // Pasar el valor obtenido de VERDE al led RGB
+    analogWrite(ledAzul, B);  // Pasar el valor obtenido de AZUL al led RGB
+    delay(300);
+  }
+
+  long findDistance(int triggerPin, int echoPin){
+    pinMode(triggerPin, OUTPUT);  
+    digitalWrite(triggerPin, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(triggerPin, LOW);
+    pinMode(echoPin, INPUT);
+    long distance = pulseIn(echoPin, HIGH, 50000)/58.2;
+    return distance;
+  } 
+
+  bool IsBall(){
+    float red, green, blue;
+    tcs.setInterrupt(false);
+    delay(60);  
+    tcs.getRGB(&red, &green, &blue);
+    tcs.setInterrupt(true);  
+  /* analogWrite(ledRojo, red);
+    analogWrite(ledVerde, green);
+    analogWrite(ledAzul, blue);*/
+    Serial.print(red); Serial.print(" "); Serial.print(green); Serial.print(" "); Serial.println(blue);
+    return blue > green && red < blue;
+  }
+
+  void Drive(int left, int right){
+    //Set Motor Direction
+    if(left > 0){
+      digitalWrite(LeftA, HIGH);
+      digitalWrite(LeftB, LOW);
+    }else if(left < 0){
+      digitalWrite(LeftA, LOW);
+      digitalWrite(LeftB, HIGH);
+    }
+    else{
+      digitalWrite(LeftA, LOW);
+      digitalWrite(LeftB, LOW);
+    }
+
+    if(right > 0){
+      digitalWrite(RightA, HIGH);
+      digitalWrite(RightB, LOW);
+    }else if(right < 0){
+      digitalWrite(RightA, LOW);
+      digitalWrite(RightB, HIGH);
+    }
+    else{
+      digitalWrite(RightA, LOW);
+      digitalWrite(RightB, LOW);
+    }
+
+    //Set Motor Velocity
+    left = abs(left);
+    right = abs(right);
+    left = left > 1 ? 1: left;
+    right = right > 1 ? 1: right;
+
+    int LeftWheel = map(left, 0, 1, 0, 255);
+    int RightWheel = map(right, 0, 1, 0, 255);
+    analogWrite(LeftPow, LeftWheel);
+    analogWrite(RightPow, RightWheel);
+  }
+
+  void UpdateInfraRedSensors(){
+      for(int i = 0; i < NInfraRedSensor; i++){
+        InfraRedValues[i] = digitalRead(InfraRedSensors[i]);
+        Serial.print(InfraRedValues[i]);
+        Serial.print(" ");
+      }
+      Serial.print("\n");
+  }
+
+  bool DetectLine(){
+    //Se puede hacer con la lista de valores, pero siento que seria un tanto peligroso
     for(int i = 0; i < NInfraRedSensor; i++){
-      InfraRedValues[i] = digitalRead(InfraRedSensors[i]);
-      Serial.print(InfraRedValues[i]);
-      Serial.print(" ");
+      if(digitalRead(InfraRedSensors[i]) != LOW){
+        return true;
+      }
     }
-    Serial.print("\n");
-}
-
-bool DetectLine(){
-  //Se puede hacer con la lista de valores, pero siento que seria un tanto peligroso
-  for(int i = 0; i < NInfraRedSensor; i++){
-    if(digitalRead(InfraRedSensors[i]) != LOW){
-      return true;
-    }
+    return false;
   }
-  return false;
-}
 
-void GoFront(){
-  LeftPulses = 0;
-  RightPulses = 0;
-  int blockDist = 15000;   //Ponemos operaciones para ponerlo en terminos de bloques
-  int RightError ;
-  int LeftError ;
-  int kp = 0.1;
-  while(LeftPulses < blockDist || RightPulses < blockDist){
-    int RightError = (blockDist - RightPulses) ;
-    int LeftError = (blockDist - LeftPulses);
-    //El error puede ser cambiado entre calcular la distancia hacia la pared o nomas con los encoders
+  void GoFront(double dist){
+    LeftPulses = 0;
+    RightPulses = 0;
+    int blockDist = dist/3.14/6*pulses_per_turn;   //Ponemos operaciones para ponerlo en terminos de bloques
+    int RightError = 0 ;
+    int LeftError = 0;
+    int kp = 0.5;
+    while(LeftPulses < blockDist || RightPulses < blockDist){
+      int RightError = (blockDist - RightPulses) ;
+      int LeftError = (blockDist - LeftPulses);
+      LeftError = LeftError < 0 ? 0 : LeftError;
+      RightError = RightError < 0 ? 0 : RightError;
+      //El error puede ser cambiado entre calcular la distancia hacia la pared o nomas con los encoders
+      Drive(RightError * kp, LeftError * kp);
 
-    if(LeftPulses < blockDist && RightPulses < blockDist){
-      Drive(1,1);
     }
-    else if(LeftPulses < blockDist){
-      Drive(kp*RightError,0);
-    }else{
-      Drive(0,kp*LeftError);
+    delay(5000);
+  }
+
+  void ActivateServos(int Limit){
+    Serial.print("Adelante");
+    for(int i = -10; i < Limit; i+=10){
+      Lservo.write(-i);
     }
+    delay(300);
   }
-  //Delay(5000)
-}
-
-void ActivateServos(int Limit){
-  Serial.print("Adelante");
-  for(int i = -10; i < Limit; i+=10){
-
-    Lservo.write(-i);
-
-  }
-  delay(300);
-
-
-}
-  // - Distance sensors
-        long FrontDistance(){
-          return findDistance(FrontDistSensorTrigger, FrontDistSensorEcho);
-        }
-        long LeftDistance(){
-          return findDistance(LeftDistSensorTrigger, LeftDistSensorEcho);
-        }
-        long RightDistance(){
-          return findDistance(RightDistSensorTrigger, RightDistSensorEcho);
-        }
+    // - Distance sensors
+          long FrontDistance(){
+            return findDistance(FrontDistSensorTrigger, FrontDistSensorEcho);
+          }
+          long LeftDistance(){
+            return findDistance(LeftDistSensorTrigger, LeftDistSensorEcho);
+          }
+          long RightDistance(){
+            return findDistance(RightDistSensorTrigger, RightDistSensorEcho);
+          }
 
 void setup() {
   
@@ -328,7 +318,7 @@ void setup() {
       attachInterrupt(digitalPinToInterrupt(LeftEncoder), Lcount, FALLING );
   // - Servo
       //Rservo.attach(servoRight);
-      //Lservo.attach(servoLeft);
+      Lservo.attach(servoLeft);
 }
 
 
